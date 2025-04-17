f3cb457b1b0ba8c282c3da15934493e82986f0a7
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2025-04-18 00:31:46 +0300

    [ENH] Updated model download links

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,12 +1,11 @@
-
 AVAILABLE_MODELS = {
     "Mistral-7B-Instruct-v0.1 (Q4_K_M)": {
         "url": "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
         "filename": "mistral-7b-instruct-v0.1.Q4_K_M.gguf"
     },
      "Mistral-7B (Q4_K_M)": {
-        "url": "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
-        "filename": "mistral-7b.Q4_K_M.gguf"
+        "url": "https://huggingface.co/TheBloke/Mistral-7B-v0.1-GGUF/resolve/main/mistral-7b-v0.1.Q4_K_M.gguf",
+        "filename": "mistral-7b-v0.1.Q4_K_M.gguf"
     },
     "Llama-2-7B-Chat (Q4_K_M)": {
         "url": "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf",
@@ -17,12 +16,12 @@ AVAILABLE_MODELS = {
         "filename": "phi-2.Q4_K_M.gguf"
     },
     "Gemma-2B-IT (Q4_K_M)": {
-        "url": "https://huggingface.co/lmstudio-ai/gemma-2b-it-GGUF/resolve/main/gemma-2b-it-q4_k_m.gguf",
-        "filename": "gemma-2b-it-q4_k_m.gguf"
+        "url": "https://huggingface.co/TheBloke/gemma-2b-it-GGUF/resolve/main/gemma-2b-it.Q4_K_M.gguf",
+        "filename": "gemma-2b-it.Q4_K_M.gguf"
     },
     "Mixtral-8x7B-Instruct-v0.1 (Q4_K_M)": {
-        "url": "https://huggingface.co/MaziyarPanahi/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main/Mixtral-8x7B-Instruct-v0.1-GGUF.Q4_K_M.gguf",
-        "filename": "Mixtral-8x7B-Instruct-v0.1-GGUF.Q4_K_M.gguf"
+        "url": "https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main/mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf",
+        "filename": "mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf"
     },
     "CodeLlama-7B-Instruct (Q4_K_M)": {
         "url": "https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/resolve/main/codellama-7b-instruct.Q4_K_M.gguf",
